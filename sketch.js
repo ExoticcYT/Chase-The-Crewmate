61a32f528a8249550380a8e9a5b21662fd3aae4a
ExoticcYT <76571211+ExoticcYT@users.noreply.github.com>
2021-01-02 20:09:27 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -109,7 +109,7 @@ function draw(){
   
   console.log(frameCount);
   
-  if(keyDown("space") && Impostor.y >= 528 ){
+  if(keyDown("space") && Impostor.y >= 428 ){
     Impostor.velocityY = -15;
   }
   
